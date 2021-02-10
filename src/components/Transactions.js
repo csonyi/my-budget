f15a7e7a5d35edc1b1aaa7fd999d1684b9c39d8c
laszlo.bodnar <laszlo.bodnar@instructure.com>
2021-02-10 13:31:15 +0100

    added proptypes

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -1,10 +1,14 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import { Route, Switch } from "react-router-dom";
 
 import { Grid } from "@material-ui/core";
 
 import { TransactionsDayView, TransactionsWeekView } from "./TransactionViews";
+import TransactionData from "../datamodels/transaction-model";
+import CurrencyHandler from "../datamodels/currency-model";
+import AccountData from "../datamodels/account-model";
 
 export default function Transactions(props) {
   const viewProps = {
@@ -13,6 +17,8 @@ export default function Transactions(props) {
     updateTransaction: props.updateTransaction,
     removeTransaction: props.removeTransaction,
     accounts: props.accounts,
+    currencyHandler: props.currencyHandler,
+    toggleTransactionCommited: props.toggleTransactionCommited,
   };
   return (
     <Grid container direction="row" spacing={1}>
@@ -27,3 +33,13 @@ export default function Transactions(props) {
     </Grid>
   );
 }
+
+Transactions.propTypes = {
+  transactions: PropTypes.arrayOf(TransactionData),
+  accounts: PropTypes.arrayOf(AccountData),
+  addTransaction: PropTypes.func,
+  updateTransaction: PropTypes.func,
+  removeTransaction: PropTypes.func,
+  toggleTransactionCommited: PropTypes.func,
+  currencyHandler: PropTypes.instanceOf(CurrencyHandler),
+};
