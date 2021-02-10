55dd2691301ad563a0ec24c19c3f12e305f93618
laszlo.bodnar <laszlo.bodnar@instructure.com>
2021-02-10 13:32:16 +0100

    refactor for readability

--- src/datamodels/account-model.js
+++ src/datamodels/account-model.js
@@ -8,9 +8,19 @@ export default class AccountData {
     id = uuid()
   ) {
     this.name = name;
-    this.balance = initialBalance;
+    this.balance = parseFloat(initialBalance);
     this.currencyId = currencyId;
     this.id = id;
+  }
+  // TODO: more readable parameters
+  editAccount(changedValues) {
+    for (const [key, value] of Object.entries(changedValues)) {
+      this[key] = value;
+    }
+  }
+
+  changeBalanceBy(amount) {
+    this.balance += amount;
   }
 
   static defaultAccountData = new AccountData("Wallet", 0, "HUF", defaultId);
@@ -24,10 +34,7 @@ export default class AccountData {
     );
   }
 
-  // TODO: more readable parameters
-  editAccount(changedValues) {
-    for (const [key, value] of Object.entries(changedValues)) {
-      this[key] = value;
-    }
+  static getAccountById(accountId, accounts) {
+    return accounts.find((account) => account.id === accountId);
   }
 }
